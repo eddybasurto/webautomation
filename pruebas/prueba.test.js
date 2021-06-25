3e6f9a3adbe1a693de64d239d2558764d1c7cfeb
Edgar Basurto <ebasurto2005@gmail.com>
2021-06-24 19:12:03 -0500

    Web Desafio3

--- pruebas/prueba.test.js
+++ pruebas/prueba.test.js
@@ -1,37 +1,32 @@
 const puppeteer = require("puppeteer")
+const expect = require('chai').expect
 
 let browser
 let page
+var price
 
 before(async () => {
   browser = await puppeteer.launch({ headless: false })
   page = await browser.newPage()
 })
 
-describe("Mi primer prueba abrir navegador", () => {
-  it(" debe de abrir el navegador", async () => {
+describe("Prueba Desafío 3", () => {
+  it("debe de abrir el navegador", async () => {
     await page.goto('http://automationpractice.com/index.php')
     await page.waitForSelector("h1")
+    price = await page.$eval('#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-line.last-item-of-tablet-line.last-item-of-mobile-line > div > div.left-block > div > div.content_price > span', ele=>ele.textContent)
+    console.log("Precio1 Id6:"+price)
+    await page.waitFor(10000)
   })
-
-  it(" dar clic en women ", async () => {
-    await page.click('a[href="http://automationpractice.com/index.php?id_category=3&controller=category"]')
-    await page.waitForSelector('#center_column > div.content_scene_cat > div > div > span')
+  
+  it(" dar clic en add to cart ", async () => {
+    await page.click('#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-line.last-item-of-tablet-line.last-item-of-mobile-line > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default > span')
+    await page.waitForSelector('h2')
+    const price2 = await page.$eval('#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div:nth-child(2) > span', ele=>ele.textContent)
+    console.log("Precio2 Id-6:"+price2)
+    expect(price).to.be.a('string',price2)
+    await page.waitFor(10000)
   })
-
-  it(" selector css ", async () => {
-    await page.goBack()
-    await page.waitForSelector("h1")
-    await page.click('#block_top_menu > ul > li:nth-child(1) > a')
-    await page.waitForSelector('#center_column > div.content_scene_cat > div > div > span', { delay: 2000})
-  })
-
-  it(" selector xpath ", async () => {
-    await page.goBack()
-    await page.waitForSelector("h1")
-    const clixpath = await page.waitForXPath('//*[@id="block_top_menu"]/ul/li[1]', { delay: 2000})
-    clixpath.click()
-      })
 })
 
 after(async () => {
